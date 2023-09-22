92a04fb2a4c13c5427f204f5ae5a9fcf4bb294c1
Isaiah N <rinriukato@gmail.com>
2023-09-22 14:10:44 -0400

    added an activity message for Hertz

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const { mongoUrl } = require('../config.json')
+const { mongoUrl } = require('../config.json');
+const { ActivityType } = require('discord.js');
 
 module.exports = {
     name: 'ready',
@@ -12,6 +13,12 @@ module.exports = {
         } catch (err) {
             console.log(err);
         }
+
+        client.user
+            .setActivity('FFXIV - the award winning MMORPG, free up to level 70 with the critically acclaimed Stormblood expansion',
+            {
+                type: ActivityType.Streaming
+            });
 
         console.log(`Intialized and ready! ${client.user.tag} is on the air!`);
     },
